c7e804aeb1d4559469796e110d757494abc0ad8e
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-09-05 03:17:29 -0400

    Update image loading paths in images.js

--- images.js
+++ images.js
@@ -17,7 +17,7 @@ async function loadBase64Image(elementId, textFilePath) {
 
 // Load all 3 images when the page is ready
 document.addEventListener("DOMContentLoaded", () => {
-  loadBase64Image("img-profile", "pics/selfie.txt");
-  loadBase64Image("img-award", "pics/award.txt");
-  loadBase64Image("img-cat", "pics/cat.txt");
+  loadBase64Image("img-profile", "selfie.txt");
+  loadBase64Image("img-award", "award.txt");
+  loadBase64Image("img-cat", "cat.txt");
 });
